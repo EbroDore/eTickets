61e893a38a9cf9fcc2aabede74ccc1a728ec3854
Brody Bond <brody.bond3@studytafensw.edu.au>
2023-04-10 11:29:18 +1000

    Init

---
+++ eTickets/Data/ApplicationDbContext.cs
@@ -0,0 +1,18 @@
+﻿using eTickets.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace eTickets.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptionsBuilder<ApplicationDbContext> options)
+        {
+            base.OnConfiguring(options);
+        }
+
+        public DbSet<Actor> Actors { get; set; }
+        public DbSet<Cinema> Cinemas { get; set; }
+        public DbSet<Movie> Movies { get; set; }
+        public DbSet<Producer> Producers { get; set; }
+    }
+}
