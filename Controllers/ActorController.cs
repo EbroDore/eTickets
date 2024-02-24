58e6c1947698adb732492b06f7aead825f5deb85
Brody Bond <brody.bond3@studytafensw.edu.au>
2024-02-24 11:02:32 +1100

    Added orders and shopping cart

---
+++ Controllers/ActorController.cs
@@ -0,0 +1,98 @@
+﻿using eTickets.Data;
+using eTickets.Data.Services;
+using eTickets.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace eTickets.Controllers
+{
+	public class ActorController : Controller
+	{
+		private readonly IActorService _service;
+
+		public ActorController(IActorService service)
+		{
+			_service = service;
+		}
+		public async Task<IActionResult> Index()
+		{
+			var actor = await _service.GetAllAsync();
+
+			if (actor == null)
+			{
+				return NotFound();
+			}
+
+			return View(actor);
+		}
+
+		//Get: Actors/Create
+		[HttpGet]
+		public IActionResult Create()
+		{
+			return View();
+		}
+
+		[HttpPost]
+		public  async Task<IActionResult> Create([Bind("FullName, ProfilePictureURL, Bio")] Actor actor)
+		{
+			if (!ModelState.IsValid)
+			{
+				return NotFound();
+			}
+			await _service.AddAsync(actor);
+			return RedirectToAction(nameof(Index));
+
+		}
+
+
+		
+		public async Task<IActionResult> Details(int id)
+		{
+			var actor = await _service.GetByIdAsync(id);
+
+			if (actor == null)
+			{
+				return NotFound();
+			}
+			return View(actor);
+		}
+
+		[HttpGet]
+		public async Task<IActionResult> Edit(int id)
+		{
+			var actor = await _service.GetByIdAsync(id);
+			return View(actor);
+		}
+
+		[HttpPost]
+		public async Task<IActionResult> Edit(int id, [Bind("Id, FullName, ProfilePictureURL, Bio")]Actor actor)
+		{
+			if (!ModelState.IsValid)
+			{
+				return NotFound();
+			}
+			await _service.UpdateAsync(id, actor);
+			return RedirectToAction(nameof(Index));
+		}
+
+		[HttpGet]
+		public async Task<IActionResult> Delete(int id)
+		{
+			var actor = await _service.GetByIdAsync(id);
+			return View(actor);
+		}
+
+		[HttpPost]
+		public async Task<IActionResult> DeleteConfirmed(int id)
+		{
+			var actor = await _service.GetByIdAsync(id);
+			if(actor == null)
+			{
+				return NotFound();
+			}
+			await _service.DeleteAsync(id);
+			return RedirectToAction(nameof(Index));
+		}
+	}
+}
---
+++ Controllers/ProducerController.cs
@@ -0,0 +1,100 @@
+﻿using eTickets.Data;
+using eTickets.Data.Services;
+using eTickets.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace eTickets.Controllers
+{
+	public class ProducerController : Controller
+	{
+
+		private readonly IProducerService _service;
+
+        public ProducerController(IProducerService service)
+        {
+			_service = service;
+        }
+        public async Task<IActionResult> Index()
+		{
+			var allProducers = await _service.GetAllAsync();
+
+			if(allProducers == null)
+			{
+				return NotFound();
+			}
+
+			return View(allProducers);
+
+		}
+
+		//Get: Actors/Create
+		[HttpGet]
+		public IActionResult Create()
+		{
+			return View();
+		}
+
+		[HttpPost]
+		public async Task<IActionResult> Create([Bind("FullName, ProfilePictureURL, Bio")] Producer producer)
+		{
+			if (!ModelState.IsValid)
+			{
+				return NotFound();
+			}
+			await _service.AddAsync(producer);
+			return RedirectToAction(nameof(Index));
+
+		}
+
+
+
+		public async Task<IActionResult> Details(int id)
+		{
+			var actor = await _service.GetByIdAsync(id);
+
+			if (actor == null)
+			{
+				return NotFound();
+			}
+			return View(actor);
+		}
+
+		[HttpGet]
+		public async Task<IActionResult> Edit(int id)
+		{
+			var producer = await _service.GetByIdAsync(id);
+			return View(producer);
+		}
+
+		[HttpPost]
+		public async Task<IActionResult> Edit(int id, [Bind("Id, FullName, ProfilePictureURL, Bio")] Producer producer)
+		{
+			if (!ModelState.IsValid)
+			{
+				return NotFound();
+			}
+			await _service.UpdateAsync(id, producer);
+			return RedirectToAction(nameof(Index));
+		}
+
+		[HttpGet]
+		public async Task<IActionResult> Delete(int id)
+		{
+			var producer = await _service.GetByIdAsync(id);
+			return View(producer);
+		}
+
+		[HttpPost]
+		public async Task<IActionResult> DeleteConfirmed(int id)
+		{
+			var actor = await _service.GetByIdAsync(id);
+			if (actor == null)
+			{
+				return NotFound();
+			}
+			await _service.DeleteAsync(id);
+			return RedirectToAction(nameof(Index));
+		}
+	}
+}
